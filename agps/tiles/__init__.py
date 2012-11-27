adfbbebe60a27df3b2a80f37afb82dabe6c50b91
Edward Saxton <eddsax@googlemail.com>
2012-11-27 19:49:57 +0000

    Stop see being covered in undergrowth

--- agps/tiles/__init__.py
+++ agps/tiles/__init__.py
@@ -1,4 +1,4 @@
-from . import start, n1, s1, e1, w1
+from . import start, n1, s1, e1, w1, n1w1
 
 grid = {
 (0,0): start,
@@ -6,4 +6,5 @@ grid = {
 (0,-1): s1,
 (1,0): e1,
 (-1,0): w1,
+(-1,1): n1w1,
 }
---
+++ agps/tiles/n1w1.py
@@ -0,0 +1,32 @@
+"""(-1, 1)"""
+
+from agps.utils import action_prompt, move, take_words
+
+scene_contents = {'boat':1}
+
+def enter(name, inventory):
+    print("The trees open out onto a beautiful beach. Small waves lap at the "
+          "sand. Ahead of you, the ocean stretches as far as you can see.")
+    if scene_contents.get('boat'):
+        print("There is a small boat high up on the sand")
+    print()
+    while True:
+        action = action_prompt(inventory)
+        if action[0] in take_words and 'boat' in action:
+            pick_up_boat(inventory)
+        elif action[0] in move:
+            if action[1][0] == 'w' and not inventory.get['boat']:
+                print("you've forgotten how to swim")
+            else:
+                return action[1]
+        else:
+            print("Sorry, I don't understand.")
+
+
+def pick_up_boat(inventory):
+    if scene_contents['boat'] > 0:
+        inventory['boat'] += 1
+        scene_contents['boat'] -= 1
+        print("You put the boat in your bag. It's a very big bag")
+    else:
+        print("You already collected the boat.")
